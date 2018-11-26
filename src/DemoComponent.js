a109ab4d1ae502d9a046c4046de04a07c3d24322
Danny Smith <hi@danny.is>
2018-11-26 23:17:25 +0000

    Add todos example

--- src/DemoComponent.js
+++ src/DemoComponent.js
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './DemoComponent.css'
 
 export default function DemoComponent() {
+  const [todos, setTodos] = useState([])
+  const [newTodo, setNewTodo] = useState('')
+
+  function handleNewTodo(e) {
+    e.preventDefault()
+    if (newTodo === '') return
+    setTodos([...todos, { text: newTodo, id: Date.now() }])
+    e.target.reset()
+  }
+
+  function handleNewTodoChange(e) {
+    setNewTodo(e.target.value)
+    e.preventDefault()
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
   return (
     <div className="demo-component">
-      <h1>Hello!</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae,
-        sit, quidem dolores totam assumenda cum maxime, fuga impedit aperiam
-        nobis veniam sed. Iusto, libero cumque doloremque error vel totam et.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae,
-        sit, quidem dolores totam assumenda cum maxime, fuga impedit aperiam
-        nobis veniam sed. Iusto, libero cumque doloremque error vel totam et.
-      </p>
+      <h1>Todos</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text} <button onClick={() => removeTodo(todo.id)}>X</button>
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={handleNewTodo}>
+        <input
+          type="text"
+          placeholder="New Todo"
+          onChange={handleNewTodoChange}
+        />
+      </form>
     </div>
   )
 }
