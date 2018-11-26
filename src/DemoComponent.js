17f137c54f0b7afbf3332213543881a6ce34ccd1
Danny Smith <hi@danny.is>
2018-11-26 19:46:56 +0000

    Initial Commit

---
+++ src/DemoComponent.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DemoComponent.css'
+
+export default function DemoComponent() {
+  return (
+    <div className="demo-component">
+      <h1>Hello!</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae,
+        sit, quidem dolores totam assumenda cum maxime, fuga impedit aperiam
+        nobis veniam sed. Iusto, libero cumque doloremque error vel totam et.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae,
+        sit, quidem dolores totam assumenda cum maxime, fuga impedit aperiam
+        nobis veniam sed. Iusto, libero cumque doloremque error vel totam et.
+      </p>
+    </div>
+  )
+}
