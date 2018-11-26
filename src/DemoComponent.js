6b799f62b3731d3f95c1100a6472c0accd20ef9a
Danny Smith <hi@danny.is>
2018-11-26 23:56:37 +0000

    Update boilerplate code

--- src/DemoComponent.js
+++ src/DemoComponent.js
@@ -4,17 +4,7 @@ import './DemoComponent.css'
 export default function DemoComponent() {
   return (
     <div className="demo-component">
-      <h1>Hello!</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae,
-        sit, quidem dolores totam assumenda cum maxime, fuga impedit aperiam
-        nobis veniam sed. Iusto, libero cumque doloremque error vel totam et.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae,
-        sit, quidem dolores totam assumenda cum maxime, fuga impedit aperiam
-        nobis veniam sed. Iusto, libero cumque doloremque error vel totam et.
-      </p>
+      <h1>👀 Hello World!</h1>
     </div>
   )
 }
